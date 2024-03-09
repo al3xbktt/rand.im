0956992043342c60a27cd30881efc31e8b1d6874
al3xbktt <85136629+al3xbktt@users.noreply.github.com>
2024-03-09 02:24:01 -0500

    FUNCTIONAL CHAT SOMEWHAT

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use("/public", express.static(__dirname + '/public'));
 app.use(cors());
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('user #' + socket.id +' connected');
   
   socket.on("sendMessage", message => {
-    console.log('message: ' + message);
+    console.log('message from ' + socket.id + ': ' + message);
   });
 
   socket.on("disconnect", () => {
-    console.log('a user disconnected');
+    console.log('user #' + socket.id + ' disconnected');
   });
 
 });
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/chatroom.html');
 });
 
-http.listen(PORT, () => {
+http.listen(PORT, 5001, () => {
   console.log(`Listening to ${PORT}`);
 });
 
